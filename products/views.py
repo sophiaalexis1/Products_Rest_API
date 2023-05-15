c95fe209158848f12ed68dc349f9c4d9486029b5
sophiaalexis1 <122618542+sophiaalexis1@users.noreply.github.com>
2023-05-15 03:19:39 -0400

    created GET function for finding products by their pk

--- products/views.py
+++ products/views.py
@@ -9,17 +9,19 @@ from .models import Product
 @api_view(['GET', 'POST'])
 def products_list(request):
 
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
-
-    # if request.method == 'GET':
-    #     products = Product.objects.all()
-    #     serializer = ProductSerializer(products, many=True)
-    #     return Response(serializer.data)
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
     
     # elif request.method == 'POST':
     #     serializer = ProductSerializer(data=request.data)
     #     serializer.is_valid(raise_exception=True)
     #     serializer.save()
     #     return Response(serializer.data, status=status.HTTP_201_CREATED)
+@api_view(['GET', 'PUT', 'DELETE'])
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
