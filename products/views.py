7a3fcfa2f0186a40d9b9cbf12f840135100fd717
sophiaalexis1 <122618542+sophiaalexis1@users.noreply.github.com>
2023-05-15 04:12:00 -0400

    updated url path with / + added required status codes

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status 
 from .serializers import ProductSerializer
 from .models import Product
 
@@ -25,12 +25,12 @@ def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     if request.method == 'GET':
         serializer = ProductSerializer(product)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == 'PUT':
         serializer = ProductSerializer(product, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == 'DELETE':
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
