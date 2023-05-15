ea7a6b6557b6736c55751a5d696badf13543bd22
sophiaalexis1 <122618542+sophiaalexis1@users.noreply.github.com>
2023-05-15 02:15:16 -0400

    updated project urls + created first view function + created products app urls file

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import ProductSerializer
+from .models import Product
 
 # Create your views here.
+@api_view(['GET'])
+def products_list(request):
+
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
