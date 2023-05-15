bf0d34235abf76c3af911e978812134b3e30f051
sophiaalexis1 <122618542+sophiaalexis1@users.noreply.github.com>
2023-05-15 03:33:54 -0400

    created DELETE request

--- products/views.py
+++ products/views.py
@@ -31,3 +31,6 @@ def product_detail(request, pk):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
