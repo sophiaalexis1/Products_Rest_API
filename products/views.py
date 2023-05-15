d7a1e3ec1f27fe3580420554db2fa12f15f893fd
sophiaalexis1 <122618542+sophiaalexis1@users.noreply.github.com>
2023-05-15 03:22:43 -0400

    Created Post request

--- products/views.py
+++ products/views.py
@@ -14,11 +14,12 @@ def products_list(request):
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
     
-    # elif request.method == 'POST':
-    #     serializer = ProductSerializer(data=request.data)
-    #     serializer.is_valid(raise_exception=True)
-    #     serializer.save()
-    #     return Response(serializer.data, status=status.HTTP_201_CREATED)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
 @api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
